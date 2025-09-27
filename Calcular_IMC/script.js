defa01696f41f6226fe12085e6d4756a771eee33
chairass <166766696+chairass@users.noreply.github.com>
2025-09-27 00:49:41 -0300

    Contador de dias

---
+++ Calcular_IMC/script.js
@@ -0,0 +1,7 @@
+const resultado = document.querySelector('#resultado')
+const altura = document.querySelector('#altura')
+const peso = document.querySelector('#peso')
+
+const calcIMC = () => {
+    if (altura.value ==)
+}
