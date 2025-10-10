a17590c966d7900a157618cc0fa432378348f40c
chairass <166766696+chairass@users.noreply.github.com>
2025-10-09 23:18:08 -0300

    Analisar Arrays

--- Analisar_Array/script.js
+++ Analisar_Array/script.js
@@ -1,18 +1,18 @@
 const maiorNumeroSpana = document.getElementById('maiorNumero')
 const menorNumeroSpana = document.getElementById('menorNumero')
 const mediaNumerosSpana = document.getElementById('mediaNumeros')
+const numeros = document.getElementById('numeros')      
 
-const numeros = [1,8,3,5,9,2,4,7,6]
+const numerosArray= []
 
-function analisarArray(numeros) {
-    maiorNumeroSpana.innerText = Math.max(...numeros)
-}
+numerosArray.push(numeros.value.split(',').map(Number))
 
+const maiorNumero = numerosArray.find (n => n === Math.max(...numerosArray))
+const menorNumero = numerosArray.find (n => n === Math.min(...numerosArray))
+const mediaNumeros = numerosArray.reduce((a,b) => a + b, 0) / numeros.length
 
-const maiorNumero = numeros.find (n => n === Math.max(...numeros))
-const menorNumero = numeros.find (n => n === Math.min(...numeros))
-const mediaNumeros = numeros.reduce((a,b) => a + b, 0) / numeros.length
-
+console.log(numerosArray)
 console.log(mediaNumeros)
 console.log(maiorNumero)
 console.log(menorNumero)
+
